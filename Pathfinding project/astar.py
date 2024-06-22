0457906200041f274d3ab8b61a5ef6d4497d2358
msd2345 <manmeet.at@gmail.com>
2024-06-22 13:50:49 -0400

    Created the Node class, added the colours. Then created methods for the variables to make them accesible. Then assigned method functions for each colour. Also started the methods for the Pygame window and compairson tool.

--- Pathfinding project/astar.py
+++ Pathfinding project/astar.py
@@ -16,3 +16,69 @@ PURPLE = (128, 0, 128)
 ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
+
+class Node:
+    #Method for Variables
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.colour = WHITE
+        self.neighbours = []
+        self.width = width
+        self.total_rows = total_rows
+
+    #Method for position
+    def get_pos(self):
+        return self.row, self.col
+    
+    #Method for closed node
+    def is_closed(self):
+        return self.colour == RED
+    
+    #Method for open node
+    def is_open(self):
+        return self.colour == GREEN
+    
+    #Method for obstacle node    
+    def is_barrier(self):
+        return self.colour == BLACK
+    
+    #Method for starting node colour
+    def is_start(self):
+        return self.colour == ORANGE
+    
+    #Method for end node colour
+    def is_end(self):
+        return self.colour == PURPLE
+    
+    #Method for restting
+    def reset(self):
+        self.colour == WHITE
+
+    def make_closed(self):
+        self.colour = RED
+
+    def make_open(self):
+        self.colour = GREEN
+    
+    def make_barrier(self):
+        self.colour = BLACK
+    
+    def make_end(self):
+        self.colour = TURQUOISE
+
+    def make_path(self):
+        self.colour = PURPLE
+    
+    #Method for the pygame window
+    def draw(self, win):
+        pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.width))
+
+    def update_neighbours(self, grid):
+        pass
+    
+    #Method to compare the values for patthing
+    def __lt__(self, other):
+        return False
